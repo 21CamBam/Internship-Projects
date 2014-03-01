930f55328052f98ea3526963f620babb8a2c2d1d
Cammi Smith <cammi.smith@email.wsu.edu>
2014-03-01 03:09:47 -0800

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <string.h>
+#include <string>
+#define INVALID_BOUNDS -1
+#define KEY_NOT_FOUND -1
+#define ORIGINAL 0
+#define REVERSED 1
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
